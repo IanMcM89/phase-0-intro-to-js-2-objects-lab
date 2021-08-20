39563e18f4355c63fbeac0d4d2b875efb45f809c
Ian <ianmcm.89@gmail.com>
2021-08-20 10:14:50 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,35 +1,20 @@
 const employee = {
-    name: 'Ian McManus',
-    streetAddress: {
-        number: '10475',
-        street: 'Gandy Blvd N',
-        unit: 'Unit 3315',
-        city: 'St. Petersburg',
-        state: 'FL',
-        zipCode: '33702'
-    }
-};
+    name: 'Ian',
+    streetAddress: '10475 Gandy Blvd North'
+}
 
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-    const newEmployee = { ...employee };
+    const newEmployee = {...employee};
     newEmployee[key] = value;
     return newEmployee;
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return employee;
-}
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => { employee[key] = value; return employee };
 
 function deleteFromEmployeeByKey(employee, key) {
-    const newEmployee = { ...employee };
+    const newEmployee = {...employee};
     delete newEmployee[key];
     return newEmployee;
 }
 
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key];
-    return employee
-}
-
-//console.log(destructivelyDeleteFromEmployeeByKey(employee, 'name'));
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => { delete employee[key]; return employee };
